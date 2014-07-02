397687f08fc6175291eec1c5363f3b2280fc3624
adamrenklint <adam@renklint.com>
2014-07-02 11:38:38 +0200

    support linking to urls

--- test/formatter.js
+++ test/formatter.js
@@ -1,12 +1,19 @@
 var formatter = require('../lib/formatter');
 var expect = require('chai').expect;
 
+function testFormat (type, before, after) {
+
+  it('should support ' + type + ' syntax', function () {
+
+    var formatted = formatter(before);
+    expect(formatted).to.equal(after);
+  });
+}
+
 describe('formatter', function () {
 
-  it('should format strings as bold', function () {
+  testFormat('bold', 'asdf **foo** asdf', 'asdf <strong>foo</strong> asdf');
 
-    var formatted = formatter('asdf **foo** asdf');
-    var expected = 'asdf <strong>foo</strong> asdf';
-    expect(formatted).to.equal(expected);
-  });
+  var linked = 'asdf <a href="foo://foo.foo/foo">foo</a> asdf';
+  testFormat('links', 'asdf [foo](foo://foo.foo/foo) asdf', linked);
 });
